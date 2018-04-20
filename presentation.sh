cf922ab5a99833b35f2e63b5c25d2a89b74d7724
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-04-20 07:54:18 -0700

    add google images breakout easter egg

---
+++ presentation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chrome http://bit.ly/search_atari_breakout --incognito --start-maximized
+# uses chrome-cli from npm to easily create incognito window with atari breakout for Google Images
